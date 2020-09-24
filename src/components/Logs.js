87f4dafd15b337b25cb4020b34b53656574de44a
akachukwu <akachi_benji@yahoo.com>
2020-09-24 12:19:02 +0100

    added styles

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import icon from "./icon.svg";
 // import Barchart from "./Barchart";
 import {
   BarChart,
   ResponsiveContainer,
   Bar,
-  Label,
-  LabelList,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -24,6 +23,8 @@ const Logs = () => {
     LiveWrapper: 10,
     TestWrapper: 10,
   });
+
+  const [empty, setEmpty] = useState(true);
 
   //CHART INFO
   const data = [
@@ -74,8 +75,11 @@ const Logs = () => {
     setYear(value);
   };
 
+  let btnState = months === "" || year === "" ? true : false;
+
   const fetchAnalysis = async () => {
-    console.log(months, year);
+    setEmpty(false);
+    // console.log(months, year);
     const response = await axios.post(
       "https://auto-response-mail-backend.herokuapp.com/analytics",
       {
@@ -88,56 +92,74 @@ const Logs = () => {
     const Servers = response.data.serverDownCount;
 
     setInfo({
-      Surge: Servers.Surge,
-      GTAssistant: Servers.GTAssistant,
-      GTCommunity: Servers.GTCommunity,
-      LiveWrapper: Servers.LiveWrapper,
-      TestWrapper: Servers.TestWrapper,
+      Surge: Servers.Surge ? Servers.Surge : "0",
+      GTAssistant: Servers.GTAssistant ? Servers.GTAssistant : "0",
+      GTCommunity: Servers.GTCommunity ? Servers.GTCommunity : "0",
+      LiveWrapper: Servers.LiveWrapper ? Servers.LiveWrapper : "0",
+      TestWrapper: Servers.TestWrapper ? Servers.TestWrapper : "0",
     });
   };
 
   return (
     <div className="container">
       <div
-        className="row d-flex align-items-center justify-content-center"
+        className="row d-flex align-items-center justify-content-center text-center"
         style={{
           marginTop: "5%",
-          backgroundColor: "rgb(255, 117, 37)",
-          padding: "0.6rem",
         }}
       >
-        <div className="col-md-3">
-          <select
-            class="form-control form-control-sm"
-            style={{ border: "1px solid rgb(255, 117, 37)", borderRadius: "0" }}
-            value={months}
-            onChange={optionsMonthsChange}
-          >
-            <option defaultValue>Select Month --</option>
-            <option value="January">January</option>
-            <option value="February">February</option>
-            <option value="March">March</option>
-            <option value="April">April</option>
-            <option value="May">May</option>
-            <option value="June">June</option>
-            <option value="July">July</option>
-            <option value="August">August</option>
-            <option value="September">September</option>
-            <option value="October">October</option>
-            <option value="November">November</option>
-            <option value="December">December</option>
-          </select>
-        </div>
-        <div className="col-md-3">
-          <select
-            class="form-control form-control-sm"
-            style={{ border: "1px solid rgb(255, 117, 37)", borderRadius: "0" }}
-            value={year}
-            onChange={optionsYearChange}
-          >
-            <option defaultValue>Select Year --</option>
-            <option value="2020">2020</option>
-          </select>
+        <div
+          className="col-md-8 col-12"
+          style={{
+            backgroundColor: "rgb(255, 117, 37)",
+            padding: "0.6rem",
+            boxShadow: "0px 0px 6px 3px rgba(0, 0, 0, 0.3)",
+            borderRadius: "3px",
+          }}
+        >
+          <div className="col-md-12 col-12 mb-1">
+            <select
+              className="form-control form-control-sm"
+              style={{
+                borderRadius: "0",
+                border: "1px solid rgb(255, 117, 37)",
+                backgroundColor: "#14171A",
+                color: "white",
+              }}
+              value={months}
+              onChange={optionsMonthsChange}
+            >
+              <option defaultValue>Select Month --</option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </select>
+          </div>
+          <div className="col-md-12 col-12 ">
+            <select
+              className="form-control form-control-sm"
+              style={{
+                border: "1px solid rgb(255, 117, 37)",
+                borderRadius: "0",
+                backgroundColor: "#14171A",
+                color: "white",
+              }}
+              value={year}
+              onChange={optionsYearChange}
+            >
+              <option defaultValue>Select Year --</option>
+              <option value="2020">2020</option>
+            </select>
+          </div>
         </div>
       </div>
       {/* BUTTON */}
@@ -146,29 +168,32 @@ const Logs = () => {
         style={{}}
       >
         <div
-          className="col-3 text-center mt-5"
+          className="col-3 text-center mt-3"
           style={{
             fontWeight: "100",
           }}
         >
           <button
-            className="botn"
+            className="botnAny"
+            disabled={btnState}
             style={{
               fontWeight: "100",
+              //   border: "1px solid white",
             }}
             onClick={fetchAnalysis}
           >
-            <span className="sendadd">
+            <span>
               Get Analysis &nbsp;{" "}
-              <i class="fa fa-search" aria-hidden="true"></i>
+              <i className="fa fa-search" aria-hidden="true"></i>
             </span>
           </button>
         </div>
       </div>
       {/* CHART */}
-      <div className="row d-flex align-items-center justify-content-center mt-5">
+
+      {empty ? (
         <div
-          className="card cardsBlack2 d-flex align-items-center justify-content-center p-auto"
+          className="row d-flex justify-content-center align-items-center"
           style={{
             paddingTop: "3rem",
             paddingRight: "1rem",
@@ -176,42 +201,71 @@ const Logs = () => {
             paddingBottom: "2rem",
           }}
         >
-          <ResponsiveContainer width={500} height={300}>
-            <BarChart
-              //   width={300}
-              //   height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 10,
-                left: 10,
-                bottom: 30,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+          <div
+            className="col-12 col-md-8  d-flex justify-content-center"
+            style={{}}
+          >
+            <img
+              className="iconAnm"
+              src={icon}
+              alt="search-icon"
+              style={{ maxWidth: "100%", height: "15rem" }}
+            />
+          </div>
 
-              <YAxis
-                label={{
-                  value: "No. of times down",
-                  angle: -90,
-                  position: "insideLeft",
-                }}
-              />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="Surge" fill="#8884d8" />
-
-              <Bar dataKey="GTAssistant" fill="#F47560" />
-
-              <Bar dataKey="GTCommunity" fill="#CCBF4D" />
-
-              <Bar dataKey="TestWrapper" fill="#61CDBB" />
-              <Bar dataKey="LiveWrapper" fill="#E8A838" />
-            </BarChart>
-          </ResponsiveContainer>
+          <div className="col-8 col-md-6 mt-5 d-flex justify-content-center">
+            {" "}
+            <h5>NO RECORDS FOUND</h5>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="row d-flex align-items-center justify-content-center mt-3">
+          <div
+            className="card cardsBlack2 d-flex align-items-center justify-content-center p-auto"
+            style={{
+              paddingTop: "3rem",
+              paddingRight: "1rem",
+              paddingLeft: "1rem",
+              paddingBottom: "2rem",
+            }}
+          >
+            <ResponsiveContainer width={500} height={300}>
+              <BarChart
+                //   width={300}
+                //   height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 30,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+
+                <YAxis
+                  label={{
+                    value: "No. of times down",
+                    angle: -90,
+                    position: "insideLeft",
+                  }}
+                />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="Surge" fill="#8884d8" />
+
+                <Bar dataKey="GTAssistant" fill="#F47560" />
+
+                <Bar dataKey="GTCommunity" fill="#CCBF4D" />
+
+                <Bar dataKey="TestWrapper" fill="#61CDBB" />
+                <Bar dataKey="LiveWrapper" fill="#E8A838" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
