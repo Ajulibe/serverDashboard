a06620ddf97c0ea683202d16bff41348d5b43e8a
akachukwu <akachi_benji@yahoo.com>
2020-09-25 16:25:31 +0100

    added styles

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import icon from "./icon.svg";
 // import Barchart from "./Barchart";
@@ -23,8 +23,13 @@ const Logs = () => {
     LiveWrapper: 10,
     TestWrapper: 10,
   });
+  const [content, setContent] = useState("");
 
   const [empty, setEmpty] = useState(true);
+
+  const addBtn = useRef();
+  const firstBtn = useRef();
+  const secondBtn = useRef();
 
   //CHART INFO
   const data = [
@@ -88,9 +93,38 @@ const Logs = () => {
       }
     );
 
-    console.log(response.data.serverDownCount);
+    console.log(response.data.mainData);
     const Servers = response.data.serverDownCount;
+    const mainData = response.data.mainData;
+    const row = mainData.map((data) => {
+      return (
+        <tr key={data._id}>
+          <td
+            style={{
+              fontSize: "0.6rem",
+            }}
+          >
+            {data.serverName}
+          </td>
+          <td
+            style={{
+              fontSize: "0.6rem",
+            }}
+          >
+            {data.date}
+          </td>
+          <td
+            style={{
+              fontSize: "0.6rem",
+            }}
+          >
+            {data.time}
+          </td>
+        </tr>
+      );
+    });
 
+    setContent(row);
     setInfo({
       Surge: Servers.Surge ? Servers.Surge : "0",
       GTAssistant: Servers.GTAssistant ? Servers.GTAssistant : "0",
@@ -100,8 +134,18 @@ const Logs = () => {
     });
   };
 
+  const addClassFn = () => {
+    console.log();
+    const element = addBtn.current;
+    const btn1 = firstBtn.current;
+    const btn2 = secondBtn.current;
+    element.classList.toggle("showMe");
+    btn1.classList.toggle("showMe");
+    btn2.classList.toggle("showMe");
+  };
+
   return (
-    <div className="container">
+    <div className="container" style={{ fontSize: "1rem" }}>
       <div
         className="row d-flex align-items-center justify-content-center text-center"
         style={{
@@ -109,12 +153,13 @@ const Logs = () => {
         }}
       >
         <div
-          className="col-md-8 col-12"
+          className="col-md-7 col-10"
           style={{
-            backgroundColor: "rgb(255, 117, 37)",
+            backgroundColor: "#1D2022",
+            border: "0.3px solid rgb(255, 117, 37)",
             padding: "0.6rem",
-            boxShadow: "0px 0px 6px 3px rgba(0, 0, 0, 0.3)",
-            borderRadius: "3px",
+            // boxShadow: "0px 0px 6px 3px rgba(0, 0, 0, 0.3)",
+            // borderRadius: "3px",
           }}
         >
           <div className="col-md-12 col-12 mb-1">
@@ -122,7 +167,7 @@ const Logs = () => {
               className="form-control form-control-sm"
               style={{
                 borderRadius: "0",
-                border: "1px solid rgb(255, 117, 37)",
+                border: "none",
                 backgroundColor: "#14171A",
                 color: "white",
               }}
@@ -148,7 +193,8 @@ const Logs = () => {
             <select
               className="form-control form-control-sm"
               style={{
-                border: "1px solid rgb(255, 117, 37)",
+                // border: "1px solid rgb(255, 117, 37)",
+                border: "none",
                 borderRadius: "0",
                 backgroundColor: "#14171A",
                 color: "white",
@@ -183,8 +229,8 @@ const Logs = () => {
             onClick={fetchAnalysis}
           >
             <span>
-              Get Analysis &nbsp;{" "}
-              <i className="fa fa-search" aria-hidden="true"></i>
+              Search &nbsp;{" "}
+              <i className="fa fa-search hideSearch" aria-hidden="true"></i>
             </span>
           </button>
         </div>
@@ -193,9 +239,9 @@ const Logs = () => {
 
       {empty ? (
         <div
-          className="row d-flex justify-content-center align-items-center"
+          className="row d-flex justify-content-center align-items-center mt-md-3"
           style={{
-            paddingTop: "3rem",
+            paddingTop: "1rem",
             paddingRight: "1rem",
             paddingLeft: "1rem",
             paddingBottom: "2rem",
@@ -209,35 +255,35 @@ const Logs = () => {
               className="iconAnm"
               src={icon}
               alt="search-icon"
-              style={{ maxWidth: "100%", height: "15rem" }}
+              style={{ maxWidth: "60%", height: "15rem" }}
             />
           </div>
 
-          <div className="col-8 col-md-6 mt-5 d-flex justify-content-center">
+          <div className="col-8 col-md-6 mt-3 d-flex justify-content-center">
             {" "}
-            <h5>NO RECORDS FOUND</h5>
+            <p className="reduceMe">NO RECORDS FOUND</p>
           </div>
         </div>
       ) : (
         <div className="row d-flex align-items-center justify-content-center mt-3">
           <div
-            className="card cardsBlack2 d-flex align-items-center justify-content-center p-auto"
+            className="col-md-7 col-10 d-flex align-items-center justify-content-center p-auto"
             style={{
-              paddingTop: "3rem",
-              paddingRight: "1rem",
-              paddingLeft: "1rem",
-              paddingBottom: "2rem",
+              //   paddingTop: "3rem",
+              //   paddingRight: "1rem",
+              //   paddingLeft: "1rem",
+              //   paddingBottom: "2rem",
+              height: "20rem",
+              border: "1px solid rgb(255, 117, 37)",
             }}
           >
-            <ResponsiveContainer width={500} height={300}>
+            <ResponsiveContainer width="100%" height="80%">
               <BarChart
-                //   width={300}
-                //   height={300}
                 data={data}
                 margin={{
-                  top: 5,
-                  right: 10,
-                  left: 10,
+                  top: 0,
+                  //   right: 10,
+                  //   left: 10,
                   bottom: 30,
                 }}
               >
@@ -249,11 +295,18 @@ const Logs = () => {
                     value: "No. of times down",
                     angle: -90,
                     position: "insideLeft",
+                    fill: "white",
+                    fontSize: 10,
                   }}
                 />
                 <Tooltip />
-                <Legend />
-                <Bar dataKey="Surge" fill="#8884d8" />
+                <Legend
+                  verticalAlign="bottom"
+                  height={5}
+                  iconSize="6"
+                  iconType="circle"
+                />
+                <Bar dataKey="Surge" fill="#8884d8" fontSize="10" />
 
                 <Bar dataKey="GTAssistant" fill="#F47560" />
 
@@ -263,6 +316,48 @@ const Logs = () => {
                 <Bar dataKey="LiveWrapper" fill="#E8A838" />
               </BarChart>
             </ResponsiveContainer>
+          </div>
+
+          <div className="col-8 text-center mt-3">
+            <button
+              ref={firstBtn}
+              className="botnAny"
+              disabled={btnState}
+              style={{
+                fontWeight: "100",
+                //   border: "1px solid white",
+              }}
+              onClick={addClassFn}
+            >
+              <span>Show Table</span>
+            </button>
+            <button
+              ref={secondBtn}
+              className="botnAny showMe"
+              disabled={btnState}
+              style={{
+                fontWeight: "100",
+              }}
+              onClick={addClassFn}
+            >
+              <span>Hide Table</span>
+            </button>
+          </div>
+
+          {/* TABLE */}
+          <div className="col-md-7 col-11 mt-3 showMe" ref={addBtn}>
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered table-hover table-sm text-center">
+                <thead>
+                  <tr>
+                    <th scope="col">Server</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Time</th>
+                  </tr>
+                </thead>
+                <tbody>{content}</tbody>
+              </table>
+            </div>
           </div>
         </div>
       )}
